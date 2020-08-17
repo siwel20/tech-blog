2faa27f3c0992b278bce00271ae86903ca346037
siwel20 <plthrock@gmail.com>
2020-08-17 16:24:43 -0600

    added comment/post partials and helpers

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
@@ -17,6 +17,7 @@ const sess = {
   };
 const app = express();
 const PORT = process.env.PORT || 3001;
+const helpers = require('./utils/helpers')
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
