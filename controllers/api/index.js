23e16cb246949782c696ac22955bd508dcc2f913
siwel20 <plthrock@gmail.com>
2020-08-17 14:24:13 -0600

    added post-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express');
 
 const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
